7d0dfc3f947c793008f6907153613b4929fde11e
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-03-24 21:44:53 +0600

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+import Navbar from "./Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        
+        <Navbar></Navbar>
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
